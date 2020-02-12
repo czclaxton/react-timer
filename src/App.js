0fdb7b88786094998356c2b17d193b3b8d284b56
Connor Claxton <czclaxton@gmail.com>
2020-02-12 13:21:13 -0600

    added glow css animations, pause, play, reset

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './App.scss';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    console.log("first render", seconds);
+    if (isRunning) {
+      const id = window.setInterval(() => {
+        console.log("tick", seconds);
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+      return () => window.clearInterval(id);
+    }
+  }, [isRunning, intervalId]);
+
+  const Reset = () => {
+    setSeconds(0);
+  };
+
+  const Pause = () => {
+    setIsRunning(false);
+  };
+
+  const Play = () => {
+    setIsRunning(true);
+  };
+
   return (
     <div className="app">
-      <div className='time-circle'>
-        <div className="time">
-          00:04
-        </div>
+      <div className="time-circle">
+        <div className="time">{seconds}</div>
       </div>
       <div className="buttons">
-        <button className="play-pause">
+        <button className="play-pause" onClick={() => Play()}>
           <i className="fa fa-play fa-2x" />
         </button>
-        <button className="play-pause">
+        <button className="play-pause" onClick={() => Pause()}>
           <i className="fa fa-pause fa-2x" />
         </button>
-        <button className="reset">
+        <button className="reset" onClick={() => Reset()}>
           Reset
         </button>
       </div>
